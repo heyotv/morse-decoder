b623e7a5e95e9a6852f882187635eded44410eef
heyotv <hey.tumilovich@gmail.com>
2020-03-16 14:58:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArr = [];
+    let morseCoded = [];
+    let decodedPhrase = '';
+    for (let i = 0; i < expr.length / 10; i++) {
+        exprArr.push(expr.slice(i * 10, i * 10 + 10));
+    }
+    for (let i = 0; i < exprArr.length; i++) {
+        morseCoded[i] = [];
+        for (let j = 0; j < exprArr[i].length / 2; j++) {
+            let temp = exprArr[i].slice(j * 2, j * 2 + 2);
+            if (temp == '10') {
+                morseCoded[i].push('.');
+            }
+            if (temp == '11') {
+                morseCoded[i].push('-')
+            }
+        }
+        if (morseCoded[i].length == 0) {
+            decodedPhrase += ' ';
+        } else {
+            decodedPhrase += MORSE_TABLE[morseCoded[i].join('')];
+        }
+    }
+    return decodedPhrase;
 }
 
 module.exports = {
